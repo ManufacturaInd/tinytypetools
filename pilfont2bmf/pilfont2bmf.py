a741e0140ea5d12e56798d57c83c9a9f5016d738
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-11 21:52:51 +0100

    pilfont2bmf: Now outputs ASCII and digits

--- pilfont2bmf/pilfont2bmf.py
+++ pilfont2bmf/pilfont2bmf.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
-
+import os
 from PIL import ImageFont
 
-FONTFILE = "6x10.pil"
+from string import digits, ascii_uppercase, ascii_lowercase
+chars = digits + ascii_uppercase + ascii_lowercase
+print chars
 
+
+FONTFILE = "6x10.pil"
+DIRNAME = os.path.basename(FONTFILE).split('.')[0]
+print DIRNAME
 font = ImageFont.load(FONTFILE)
 
-w,h = font.getsize('a')
-im = font.getmask('a')
-for y in xrange(h):
-    line = ""
-    for x in xrange(w):
-        if im.getpixel((x,y)):
-            line += "#"
-        else:
-            line += "."
-    print line
+if not os.path.exists(DIRNAME):
+    os.mkdir(DIRNAME)
+
+for char in chars:
+    txtfile = open(os.path.join(DIRNAME, "%s.txt" % char), 'w')
+    lines = []
+    w,h = font.getsize(char)
+    im = font.getmask(char)
+    for y in xrange(h):
+        line = ""
+        for x in xrange(w):
+            if im.getpixel((x,y)):
+                line += "#"
+            else:
+                line += "."
+        lines.append(line + "\n")
+    txtfile.writelines(lines)
+    txtfile.close()
+    print char
