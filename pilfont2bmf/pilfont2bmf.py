844541ebc350888444af01194243cebfb2f6a1b0
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-11 22:56:08 +0100

    Punctuation and proper filename creation (should settle on a standard soon)

--- pilfont2bmf/pilfont2bmf.py
+++ pilfont2bmf/pilfont2bmf.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from PIL import ImageFont
+import unicodedata
+from htmlentitydefs import codepoint2name
 
-from string import digits, ascii_uppercase, ascii_lowercase
-chars = digits + ascii_uppercase + ascii_lowercase
+from string import digits, ascii_uppercase, ascii_lowercase, punctuation
+chars = unicode(digits + ascii_uppercase + ascii_lowercase + punctuation)
+
+# FIXME: Add PT relevant characters (pillow doesn't output them properly)
+# chars += u"áàãâéèêíìóòõôúùçÁÀÃÂÉÈÊÍÌÓÒÕÔÚÙÇ"
+
 print chars
 
 
 FONTFILE = "6x10.pil"
 DIRNAME = os.path.basename(FONTFILE).split('.')[0]
-print DIRNAME
 font = ImageFont.load(FONTFILE)
 
 if not os.path.exists(DIRNAME):
     os.mkdir(DIRNAME)
 
 for char in chars:
-    txtfile = open(os.path.join(DIRNAME, "%s.txt" % char), 'w')
+    # use ascii for the file name
+    # for punctuation, deal with it specifically with the unicode name
+    if char in punctuation:
+        charname = '_' + unicodedata.name(char)
+        filename = charname.lower().replace(' ', '').replace('-', '')
+    else:
+        # we use the HTML entities definition for these
+        try:
+            filename = '_' + codepoint2name[ord(char)]
+        except KeyError:
+            filename = char
+
+    print filename + ".txt"
+    txtfile = open(os.path.join(DIRNAME, "%s.txt" % filename), 'w')
+    char = char.encode('utf-8')
     lines = []
     w,h = font.getsize(char)
     im = font.getmask(char)
@@ -30,4 +50,3 @@ for char in chars:
         lines.append(line + "\n")
     txtfile.writelines(lines)
     txtfile.close()
-    print char
